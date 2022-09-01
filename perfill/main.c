3aed994e0be5fb1d84797d4ed436c573b50516fc
GabrielRossi <gabrieldesouzarossi123456@gmail.com>
2022-09-01 18:26:37 -0300

    01_09_2022

---
+++ perfill/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    char nome[22] = "gabriel Rossi kkkkkk";
+    int idade=18;
+    int i=0;
+    while(i<=22){
+        if(i<=14){
+            printf("%c",nome[i]);
+        }else{
+        printf("%c",nome[i]);
+        }
+    i++;
+    }
+    printf("\nidade: %d",idade);
+}
